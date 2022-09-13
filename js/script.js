2408cb5f6125bd647220a452fe38c8152f5a1260
kongguksu <104235938+kongguksu@users.noreply.github.com>
2022-09-13 13:22:48 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,83 @@
+//////////////////////////////
+///// variables
+
 const btnShare = document.querySelector(`.share-btn`);
 const speechBubble = document.querySelector(`.share-popup`);
 
-const mobileBtnShare = document.querySelector(`.mobile--share-btn`);
+const authorContainer = document.querySelector(`.author-container`);
+const authorInfo = document.querySelector(`.author-and-share`);
+
+const btnMobile = document.querySelector(`.mobile--share-btn`);
 const mobileSharePopup = document.querySelector(`.mobile--share-popup`);
 
-// btnShare.addEventListener(`click`, function () {
-//   speechBubble.classList.toggle(`hidden`);
-// });
+//////////////////////////////
+///// function
 
+const btnActive = function () {
+  if (!speechBubble.classList.contains(`hidden`)) {
+    btnShare.classList.add(`btn--active`);
+    document.querySelector(`.share-icon`).classList.add(`btn-icon--active`);
+  }
+};
+
+const btnInactive = function () {
+  if (
+    speechBubble.classList.contains(`hidden`) &&
+    mobileSharePopup.classList.contains(`hidden`)
+  ) {
+    btnShare.classList.remove(`btn--active`);
+    document.querySelector(`.share-icon`).classList.remove(`btn-icon--active`);
+  }
+};
+
+//////////////////////////////
+///// event handlers
+
+// share popup open when share button clicked
 btnShare.addEventListener(`click`, function () {
+  // Desktop
   if (window.screen.width > 608) {
-    speechBubble.classList.toggle(`hidden`);
+    speechBubble.classList.remove(`hidden`);
+    btnActive();
   } else if (window.screen.width <= 608) {
+    // Mobile
+    // hide author-and-share & show mobileSharePopup
+    authorInfo.classList.add(`hideOpacity`);
     mobileSharePopup.classList.remove(`hidden`);
+    mobileSharePopup.style.height = `${authorContainer.height}`;
   }
 });
 
+// exit out of share popup if escape key pressed
 document.addEventListener(`keydown`, function (e) {
-  if (e.key === `Escape` && !speechBubble.classList.contains(`hidden`)) {
-    speechBubble.classList.add(`hidden`);
-    btnShare.blur();
+  if (e.key === `Escape`) {
+    if (!speechBubble.classList.contains(`hidden`)) {
+      speechBubble.classList.add(`hidden`);
+      btnInactive();
+      btnShare.blur(); // remove focus when escape key pressed
+    }
+
+    // mobile
+    if (!mobileSharePopup.classList.contains(`hidden`)) {
+      mobileSharePopup.classList.add(`hidden`);
+      authorInfo.classList.remove(`hideOpacity`);
+      btnInactive();
+      btnShare.blur();
+    }
   }
 });
 
+// exit out of share popup if clicked outside
 document.addEventListener(`click`, function (e) {
   if (!btnShare.contains(e.target) && !speechBubble.contains(e.target)) {
     speechBubble.classList.add(`hidden`);
+    btnInactive();
   }
-});
 
-// mobile
-mobileBtnShare.addEventListener(`click`, function () {
-  mobileSharePopup.classList.toggle(`hidden`);
-});
-
-document.addEventListener(`keydown`, function (e) {
-  if (e.key === `Escape` && !mobileSharePopup.classList.contains(`hidden`)) {
+  // mobile
+  if (!btnMobile.contains(e.target) && !authorContainer.contains(e.target)) {
     mobileSharePopup.classList.add(`hidden`);
-    btnShare.blur();
-  }
-});
-
-document.addEventListener(`click`, function (e) {
-  if (!btnShare.contains(e.target) && !mobileSharePopup.contains(e.target)) {
-    mobileSharePopup.classList.add(`hidden`);
+    authorInfo.classList.remove(`hideOpacity`);
+    btnInactive();
   }
 });
